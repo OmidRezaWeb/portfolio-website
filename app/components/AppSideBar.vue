e42368f4ce13d1fae972a64bffa98efa2f15ffb5
Omidreza <144906057+OmidRezaWeb@users.noreply.github.com>
2025-07-26 23:57:11 +0330

    feat(components/sidebar): create side bar
    Add side bar and font size and hero image

---
+++ app/components/AppSideBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="flex flex-col items-center gap-16 pt-16">
+    <!-- Hero -->
+    <div class="flex flex-col gap-5">
+      <img src="/images/hero.png" alt="omidreza shahbazi">
+      <span class="text-center">
+        <h2 class="text-h5 font-bold">امیدرضا شهبازی</h2>
+        <h3 class="text-p">توسعه دهنده وب</h3>
+      </span>
+    </div>
+    <!-- Menu -->
+    <nav class="menu">
+      <ul class="gap-14 grid">
+        <li><NuxtLink to="/" class="text-h6 font-bold">خانه</NuxtLink></li>
+        <li><NuxtLink to="/projects" class="text-h6 font-bold">نمونه کارها</NuxtLink></li>
+        <li><NuxtLink to="/about" class="text-h6 font-bold">درباره من</NuxtLink></li>
+        <li><NuxtLink to="/contacts" class="text-h6 font-bold">راه های ارتباطی</NuxtLink></li>
+      </ul>
+    </nav>
+  </section>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="grid lg:grid-cols-12 gap-4 mx-16">
+    <AppSideBar class="col-span-2"/>
     <slot />
   </div>
 </template>
